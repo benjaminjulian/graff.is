011cd61fb4c5a6ef06fcd8127e97ce888d7fb212
benjaminjulian <benjaminjulian@protonmail.com>
2021-11-25 22:24:35 +0000

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>graff.is</title>
+    </head>
+    <body>
+        <h1>graff.is</h1>
+        <p>graffiti-vefurinn</p>
+    </body>
+</html>
