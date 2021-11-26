8b578e1893e88a1704235b6e86a8ad03ccc6acbc
benjaminjulian <benjaminjulian@protonmail.com>
2021-11-26 10:10:23 +0000

    upload fix, sql added

--- index.php
+++ index.php
@@ -1,11 +1,104 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>graff.is</title>
-    </head>
-    <body>
-        <h1>graff.is</h1>
-        <p>graffiti-vefurinn</p>
-    </body>
+ 
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=0.9">
+
+    <style>
+        html {
+            font-family: sans-serif;
+        }
+        
+        #uploadlabel {
+            border: 1px solid #ccc;
+            display: inline-block;
+            padding: 7px 15px;
+            cursor: pointer;
+            border-radius: 5px;
+            background-color: blue;
+            color: #ffffff;
+        }
+        #file_to_upload {
+            display: none;
+        }
+        .progressor {
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            float: left;
+            margin: 3px;
+        }
+ 
+    </style>
+    <title>graff.is</title>
+ 
+</head>
+ 
+<body>
+    <h1>graff.is</h1>
+    <h2>Senda inn myndir af graffi</h2>
+    <label id="uploadlabel">
+        <span>
+            Veldu myndir
+        </span>
+        <input type="file" name="file_to_upload" id="file_to_upload" class="upload" onchange="upgo()" accept=".jpg, .jpeg, .png" multiple>
+    </label>
+    <div id="progress_status"></div>
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    <script>
+    function upgo() {
+// This is input object which type of file.  
+var uploader = document.getElementById("file_to_upload"); 
+
+// We'll send a new post for each file.
+for(var i=0, j=uploader.files.length; i<j; i++)
+{
+    var uploaderForm = new FormData(); // Create new FormData
+    uploaderForm.append("action","upload"); // append extra parameters if you wish.
+    uploaderForm.append("image",uploader.files[i]); // append the next file for upload
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {       
+        if(xhr.readyState==4 && xhr.status==200)
+        {
+          document.getElementById('progress_status').innerHTML = "Komið!";
+        }
+    }
+    
+    xhr.addEventListener("loadstart", function(e){
+        console.log(e)
+        // generate unique id for progress bars. This is important because we'll use it on progress event for modifications
+        this.progressId = "progress_" + Math.floor((Math.random() * 100000)); 
+
+        
+    });
+
+    /*xhr.upload.onprogress = function(e){
+        var done = e.position || e.loaded, total = e.totalSize || e.total
+        var present = Math.floor(done/total*100)
+        
+        document.getElementById('progress_status').innerHTML = present + '%'
+    }*/
+    xhr.upload.onprogress = function(e) 
+    {
+        var completed = 0;
+        if (e.lengthComputable) {
+            var done = e.position || e.loaded,
+                total = e.totalSize || e.total;
+            
+            if (document.getElementById("nr_" + total) == null) {
+                $("#progress_status").append('<span id="nr_' + total + '" class="progressor" ></span>');
+            }
+            completed = Math.round((done / total * 1000) / 10);
+            document.getElementById('nr_' + total).innerHTML = completed + '%';
+        }
+    };
+
+    xhr.open("POST","uploado.php");
+    xhr.send(uploaderForm);
+}
+}
+    </script>
+</body>
+ 
 </html>
--- uploado.php
+++ uploado.php
@@ -1,86 +1,137 @@
 
 <?php
-require "creds.php";
+    require "../creds.php";
+    require 'vendor/autoload.php';
 
-$album = $_POST["album"];
-$file_name = $_FILES['image']['name'];   
-$temp_file_location = $_FILES['image']['tmp_name']; 
-		
-$maxDim = 300;
-list($width, $height, $type, $attr) = getimagesize( $temp_file_location );
-if ( $width > $maxDim || $height > $maxDim ) {
-    $target_filename = $file_name;
-    $ratio = $width/$height;
-    if( $ratio > 1) {
-        $new_width = $maxDim;
-        $new_height = $maxDim/$ratio;
-    } else {
-        $new_width = $maxDim*$ratio;
-        $new_height = $maxDim;
+    function getGps($exifCoord, $hemi) {
+
+        $degrees = count($exifCoord) > 0 ? gps2Num($exifCoord[0]) : 0;
+        $minutes = count($exifCoord) > 1 ? gps2Num($exifCoord[1]) : 0;
+        $seconds = count($exifCoord) > 2 ? gps2Num($exifCoord[2]) : 0;
+
+        $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+
+        return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+
     }
-    $src = imagecreatefromstring( file_get_contents( $temp_file_location ) );
-    $dst = imagecreatetruecolor( $new_width, $new_height );
-    imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
-    imagedestroy( $src );
-    $exif = exif_read_data($temp_file_location);
-    if ($exif && isset($exif['Orientation']))
-    {
-        $orientation = $exif['Orientation'];
-        if ($orientation != 1)
+
+    function gps2Num($coordPart) {
+
+        $parts = explode('/', $coordPart);
+
+        if (count($parts) <= 0)
+            return 0;
+
+        if (count($parts) == 1)
+            return $parts[0];
+
+        return floatval($parts[0]) / floatval($parts[1]);
+    }
+
+    try {
+        $exif = exif_read_data($temp_file_location);
+        var_dump($exif);
+
+        $lon = getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+        $lat = getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+        $date = $exif['DateTimeOriginal'];
+
+        $prefix = date("Y-m-d--H-i-s ", strtotime($date));
+    } catch (Exception $e) {
+        exit($e);
+    }
+
+
+
+    $file_name = $_FILES['image']['name'];   
+    $temp_file_location = $_FILES['image']['tmp_name']; 
+            
+    $maxDim = 300;
+    list($width, $height, $type, $attr) = getimagesize( $temp_file_location );
+    if ( $width > $maxDim || $height > $maxDim ) {
+        $target_filename = $file_name;
+        $ratio = $width/$height;
+        if( $ratio > 1) {
+            $new_width = $maxDim;
+            $new_height = $maxDim/$ratio;
+        } else {
+            $new_width = $maxDim*$ratio;
+            $new_height = $maxDim;
+        }
+        $src = imagecreatefromstring( file_get_contents( $temp_file_location ) );
+        $dst = imagecreatetruecolor( $new_width, $new_height );
+        imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+        imagedestroy( $src );
+        $exif = exif_read_data($temp_file_location);
+        if ($exif && isset($exif['Orientation']))
         {
-            $deg = 0;
-            switch ($orientation)
+            $orientation = $exif['Orientation'];
+            if ($orientation != 1)
             {
-                case 3:
-                    $deg = 180;
-                break;
-                case 6:
-                    $deg = 270;
-                break;
-                case 8:
-                    $deg = 90;
-                break;
-            }
-            if ($deg)
-            {
-                $dst = imagerotate($dst, $deg, 0);
-            }
-        } // if there is some rotation necessary
-        
-    } // if have the exif orientation info
-    imagejpeg( $dst, $target_filename ); // adjust format as needed
-    imagedestroy( $dst );
-}
+                $deg = 0;
+                switch ($orientation)
+                {
+                    case 3:
+                        $deg = 180;
+                    break;
+                    case 6:
+                        $deg = 270;
+                    break;
+                    case 8:
+                        $deg = 90;
+                    break;
+                }
+                if ($deg)
+                {
+                    $dst = imagerotate($dst, $deg, 0);
+                }
+            } // if there is some rotation necessary
+            
+        } // if have the exif orientation info
+        imagejpeg( $dst, $target_filename ); // adjust format as needed
+        imagedestroy( $dst );
+    }
 
-try {
-    $data = exif_read_data($temp_file_location);
-    var_dump($data);
-    $prefix = date("Y-m-d--H-i-s ", strtotime($data['DateTimeOriginal']));
-} catch (exception $e) {
-    $prefix = rand(1000000, 9999999);
-}
+    $s3 = new Aws\S3\S3Client([
+        'region'  => 'eu-west-1',
+        'version' => 'latest',
+        'credentials' => [
+            'key'    => $creds_key,
+            'secret' => $creds_secret,
+        ]
+    ]);
 
-		require '../vendor/autoload.php';
+    $result = $s3->putObject([
+        'Bucket' => 'graff',
+        'Key'    => "thumbs/".$prefix."_".$file_name,
+        'SourceFile' => $target_filename
+    ]);		
 
-		$s3 = new Aws\S3\S3Client([
-			'region'  => 'eu-west-1',
-			'version' => 'latest',
-			'credentials' => [
-				'key'    => $creds_key,
-				'secret' => $creds_secret,
-			]
-		]);		
+    $result = $s3->putObject([
+        'Bucket' => 'graff',
+        'Key'    => "fullres/".$prefix."_".$file_name,
+        'SourceFile' => $temp_file_location		
+    ]);
 
-		$result = $s3->putObject([
-			'Bucket' => 'graff',
-			'Key'    => "thumbs/".$prefix."_".$file_name,
-			'SourceFile' => $target_filename
-		]);		
+    unlink($target_filename);
 
-		$result = $s3->putObject([
-			'Bucket' => 'graff',
-			'Key'    => "fullres/".$prefix."_".$file_name,
-			'SourceFile' => $temp_file_location		
-		]);	
-		unlink($target_filename);
+    $mysqli = new mysqli("localhost", $mysql_user, $mysql_pass);
+    $mysqli -> select_db("base_db");
+    $mysqli -> query("SET time_zone = '+00:00'");
+
+    $q_filename = $mysqli -> real_escape_string($prefix."_".$file_name);
+    $q_ip_address = $mysqli -> real_escape_string($_SERVER['REMOTE_ADDR']);
+    $q_date_taken = $mysqli -> real_escape_string($date);
+    $q_latitude = $mysqli -> real_escape_string($lat);
+    $q_longitude = $mysqli -> real_escape_string($lon);
+
+
+    $q = "INSERT INTO graffiti (filename, ip_address, date_taken, date_uploaded, gps_latitude, gps_longitude) values
+            ('$filename', '$ip_address', '$q_date_taken', NOW(), $q_latitude, $q_longitude);";
+    
+    if ($mysqli -> query($q) === TRUE) {
+        echo("\n\nSUCCESS");
+    } else {
+        echo("\n\nVILLA");
+    }
 ?>
